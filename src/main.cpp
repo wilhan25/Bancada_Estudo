c990e05233df9715a8a1513c5c440038d98953a6
Wilhan Almeida <wilhan.amello@gmail.com>
2026-06-09 01:16:24 -0300

    teste de watchdog

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,31 @@
 #include <Arduino.h>
+#include <esp_task_wdt.h> // <-- 1. Importamos a biblioteca do Watchdog do ESP32
 
-void TaskMemoria(void *pv){
+void TaskEgoista(void *pv){
+  esp_task_wdt_add(NULL); //WatchDog para vigiar a tarefa
   while (1)
   {
     /* code */
-    UBaseType_t livreAntes = uxTaskGetStackHighWaterMark(NULL);
-    Serial.print("[STACK] Espaço livre iniciamlente: ");
-    Serial.println(livreAntes);
-
-    char meu_buffer[1800];
-
-    snprintf(meu_buffer, sizeof(meu_buffer),"Consumindo memória de forma controlada...");
-
-    UBaseType_t livreDepois = uxTaskGetStackHighWaterMark(NULL);
-    Serial.printf("[STACK] Espaço livre com buffer ativo: %d bytes \n", livreDepois);
-    Serial.println("--------------------------");
-
-    vTaskDelay(pdMS_TO_TICKS(3000));
-  }  
+    Serial.println("\n [TASK] Começando processamento de 8 segundo ....");
+    uint32_t tempoInicio = millis();
+    while ((millis()-tempoInicio)<8000)
+    {
+      // função para ocupar o processador por 8 seg
+    }
+    
+    Serial.println("[TASK] Terminei com Sucesso ! (Isso nunca vai aparecer)");
+    esp_task_wdt_reset();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
 }
 
 void setup()
 {
   Serial.begin(115200);
   vTaskDelay(pdMS_TO_TICKS(3000));
-  Serial.println("---Duelo pelo processador começou---");
+  Serial.println("\n--- Teste do Cão de Guarda (Watchdog) ---");
 
-
-  xTaskCreatePinnedToCore(TaskMemoria, "memoria", 2048,NULL,1,NULL,1);
+  xTaskCreatePinnedToCore(TaskEgoista,"task", 2048,NULL,3,NULL,1);
 }
 
 void loop()
