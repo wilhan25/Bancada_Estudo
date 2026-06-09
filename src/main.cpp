fe73bf04bbb53eb7352ef395ef3537f96cf0be11
Wilhan Almeida <wilhan.amello@gmail.com>
2026-06-09 00:07:44 -0300

    teste de estouro de memoria

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,23 @@
 #include <Arduino.h>
 
-void TaskImportante(void *pv)
-{
+void TaskMemoria(void *pv){
   while (1)
   {
-    Serial.println("\n[👑 NOBRE] Acordei! Eu tenho prioridade 3. Saiam da frente!");
-    Serial.println("[👑 NOBRE] Executando algo crítico...");
+    /* code */
+    UBaseType_t livreAntes = uxTaskGetStackHighWaterMark(NULL);
+    Serial.print("[STACK] Espaço livre iniciamlente: ");
+    Serial.println(livreAntes);
 
-   uint32_t tempoInicio = millis();
-        // O ESP32 vai ficar preso neste 'while' fritando o processador por 500ms
-        while ((millis() - tempoInicio) < 300) {
-            // Prende o processador aqui. A task continua em RUNNING.
-            // Nenhuma task de prioridade menor consegue respirar aqui.
-        }
+    char meu_buffer[1800];
 
-    Serial.println("[👑 NOBRE] Terminei, vou dormir por 3 segundos...");
-    // vTaskDelay(pdMS_TO_TICKS(3000));
-  }
-}
+    snprintf(meu_buffer, sizeof(meu_buffer),"Consumindo memória de forma controlada...");
 
-void TaskComum(void *pv)
-{
-  uint32_t contador = 0;
-  while (1)
-  {
-    Serial.printf("[🛠️ OPERARIA] Trabalhando... Giro número: %d\n", contador++);
-    vTaskDelay(pdMS_TO_TICKS(100));
-  }
+    UBaseType_t livreDepois = uxTaskGetStackHighWaterMark(NULL);
+    Serial.printf("[STACK] Espaço livre com buffer ativo: %d bytes \n", livreDepois);
+    Serial.println("--------------------------");
+
+    vTaskDelay(pdMS_TO_TICKS(3000));
+  }  
 }
 
 void setup()
@@ -35,9 +26,8 @@ void setup()
   vTaskDelay(pdMS_TO_TICKS(3000));
   Serial.println("---Duelo pelo processador começou---");
 
-  xTaskCreatePinnedToCore(TaskImportante,    "Nobre",    2048, NULL, 3, NULL, 1); 
-    xTaskCreatePinnedToCore(TaskComum, "Operaria", 2048, NULL, 1, NULL, 1);
 
+  xTaskCreatePinnedToCore(TaskMemoria, "memoria", 2048,NULL,1,NULL,1);
 }
 
 void loop()
